f5135cb83a7f3c479873777aa672ef5b4946ce6a
poxx1 <julianlastrama@outlook.com>
2022-12-27 09:21:54 -0300

    Agregar archivos de proyecto.

---
+++ UAI-2023-TFI/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UAI_2023_TFI.Startup))]
+namespace UAI_2023_TFI
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
